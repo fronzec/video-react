62df4eac7e2439a9e86eb43a843554d60859c20f
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-03-01 18:43:00 -0600

    Componente separado
    Renderizando un componente separado

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
+import Media from './src/playlist/components/media.js'
 
 const app = document.getElementById('app')
 const holaMundo = <h1>Hola Mundo!!!</h1>
 // ReactDOM.render(que voy a renderizar, donde lo voy a renderizar)
-ReactDOM.render(holaMundo, app)
+render(<Media/>, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Media extends React.Component {
+    render (){
+        return (
+            <div>
+                <div>
+                    <img width={260} height={160} />
+                    <h3>¿Por qué aprender React?</h3>
+                    <p>Eduardo Flores</p>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Media
