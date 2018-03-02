2743f84a2e13626df0ee2d1e62adc62f528f8c0e
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-03-01 19:31:43 -0600

    Enlazando elementos del DOM
    Enlazando el evento onClick a un componente de react
    
    Leer la documentacion de ReactEvents
    https://reactjs.org/docs/handling-events.html

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -4,6 +4,15 @@ import PropTypes from 'prop-types'
 
 class Media extends React.Component {
 
+    // constructor(props){
+    //     super(props)
+    //     this.handleClick = this.handleClick.bind(this)///Cambio del contexto al metodo handleClick
+    // }
+
+    ///Los arrow function heredan el contexto de su padre automaticamente en vez de hacer el bind a cada metodo
+    handleClick= (event) =>{
+        console.log(this.props.tittle)
+    }
     render() {
         ///Extrayendo los valores a propiedades
         const {title, author, image} = this.props;
@@ -19,7 +28,7 @@ class Media extends React.Component {
             }
         }
         return (
-            <div className={'Media'}>
+            <div className={'Media'} onClick={this.handleClick}>
                 <div className={'Media-cover'}>
                     <img className={'Media-image'} src={this.props.image} width={260} height={160}/>
                     <h3 className={'Media-title'}>{this.props.title}</h3>
@@ -32,7 +41,7 @@ class Media extends React.Component {
 ///Usamos prop-types para validar los tipos de datos que estan llegando al componente, se muestran como warnings en la consola del navegador
 Media.propTypes = {
     image: PropTypes.string,
-    tittle: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['video','audio'])///Valida que el valor esperado este dentro de un conjunto de valores
 }
