fd57a9e579454e0b1edeaaa9912d1ce65672d4ad
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-03-01 18:15:44 -0600

    Primer componente renderizado usando react
    Primer componente renderizado usando react

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = document.getElementById('app')
+const holaMundo = <h1>Hola Mundo!!!</h1>
+// ReactDOM.render(que voy a renderizar, donde lo voy a renderizar)
+ReactDOM.render(holaMundo, app)
