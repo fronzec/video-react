2911dc2c3e65f442921d18e06b90dc7adabf9b26
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-03-01 19:02:03 -0600

    Estilos desde archivos css
    Estilos desde archivos css

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './media.css'
+
 
 class Media extends React.Component {
     render (){
@@ -13,11 +15,11 @@ class Media extends React.Component {
             }
         }
         return (
-            <div style={styles.container}>
-                <div>
-                    <img src='./src/images/covers/bitcoin.jpg' width={260} height={160} />
-                    <h3>¿Por qué aprender React?</h3>
-                    <p>Eduardo Flores</p>
+            <div className={'Media'}>
+                <div className={'Media-cover'}>
+                    <img className={'Media-image'} src='./src/images/covers/bitcoin.jpg' width={260} height={160} />
+                    <h3 className={'Media-title'}>¿Por qué aprender React?</h3>
+                    <p className={'Media-author'}>Eduardo Flores</p>
                 </div>
             </div>
         )
